bbb616ea4f4164a6526bfb3c0b31e5b8c39be627
Chris Gu <cjg245@cornell.edu>
2021-10-22 01:01:26 -0400

    initialize

---
+++ server.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA5qbi4oeZjhiymh7wQMOdCEbkUV7M83rc",
+  authDomain: "carpool-db0ae.firebaseapp.com",
+  projectId: "carpool-db0ae",
+  storageBucket: "carpool-db0ae.appspot.com",
+  messagingSenderId: "106745544381",
+  appId: "1:106745544381:web:eb97ee1d688b5a12cdc1d4",
+  measurementId: "G-NKJLPH10GX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
